aa4fa5e34bf7b83305eea2d056395395c206c100
Sunghyun Park <cmos00@nate.com>
2019-02-09 16:14:51 +0900

    20190209 구조체

---
+++ EmpTest/EmpTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+struct Emp //구조체
+{
+    public int id;
+    public string name;
+}
+
+class EmpTest
+{
+    public static void Main(string[] args)
+    {
+        Emp e;  //선언
+        e.id = 1;
+        e.name = "1길동";
+        Console.WriteLine("사번: {0}", e.id);
+        Console.WriteLine("이름: {0}", e.name);
+
+        Emp e1 = new Emp(); //new하더라도 스택에 저장 , 선언
+        e1.id = 2;
+        e1.name = "2길동";
+        Console.WriteLine("사번: {0}", e1.id);
+        Console.WriteLine("이름: {0}", e1.name);
+    }
+}
+
