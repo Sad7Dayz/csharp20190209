47f6e77830e6e325f666c325fce5c4ebd4f7e3b3
Sunghyun Park <cmos00@nate.com>
2019-02-09 13:11:46 +0900

    20190209 강의소스 C# 변수의 형 변환

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        int i; //멤버, 필드, 인스턴스변수
+        //System.Int32 i; int i와 같음
+        static int j; //멤버,변수, 클래스 변수
+        static void Main(string[] args)//멤버 메소드
+        {
+            string name = "홍길동";
+            int k = 99;
+            Console.WriteLine($"Hello {name}"); //새로나온기법
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello {0}{1}", name, k);
+            //system = namespace(분류이름 - 클래스의 소속)
+        }
+    }
+}
---
+++ TypeCastExample/TypeCastExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class TypeCastExample1
+{
+    public static void Main()
+    {
+        short a = 10;
+        int b = a;          //short형은 int형에 묵시적으로 변환되어 대입
+        int c = 50000;      //이진수"0000 0000 0000 0000 1100 0011 0101 0000" 표현
+                            //short형으로 변환하면 부호비트가 0에서 1로, 음수가 된다.
+        try
+        {
+            short d = checked((short)c); //명시적으로 형변환하지 않으면 오류, 오버플로우 검사하여 예외 발생.
+            Console.WriteLine("Short: {0}", d);
+        }
+        catch (Exception e) //모든에러의 예외 받기
+        {
+            Console.WriteLine(e.Message);   //에러 메세지
+            Console.WriteLine(e.StackTrace);//에러 발생된 줄
+        }
+    }
+}
