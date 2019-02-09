6724bb36913f394e964c4d155c3b2fb2d16f6b22
Sunghyun Park <cmos00@nate.com>
2019-02-09 15:06:39 +0900

    20190209 논리연산자

---
+++ Emp/Emp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Emp
+{
+
+}
+class Programmer:Emp
+{
+
+}
+class EmpTest
+{ 
+    static void Main(string[] args)
+    {
+        Emp e = new Emp();
+        Programmer p = new Programmer();
+        Emp e1 = p as Emp;
+        Emp e2 = (Emp)p;
+
+            //if(p is Emp) //is는 객체 타입을 비교 연산자
+            //{
+            //    Console.WriteLine("Progrmmer is Emp");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("programmer is not emp");
+            //}
+    }
+}
