bb4c8541ddeb2b6d9c3112f568ba90ad7d55aeee
Sunghyun Park <cmos00@nate.com>
2019-02-10 16:41:24 +0900

    20190210 값전달, 참조전달

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        //static void Swap(int a, int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine("a={0}, b={1}", a, b);
+            Swap(ref a, ref b);
+            //Swap( a, b);
+            Console.WriteLine("a={0}, b={1}", a, b);
+        }
+    }
+}
