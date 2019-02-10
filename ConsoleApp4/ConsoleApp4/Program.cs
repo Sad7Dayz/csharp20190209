afd355b162299f00fe47fb697f2943c12f1d0aaf
Sunghyun Park <cmos00@nate.com>
2019-02-10 10:48:32 +0900

    20190210 변수 속성

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PropertyTest
+{
+    class Emp : Object      //클래스의 제일 위를 object라 한다. 
+    {
+        public int Empno { get; set; }
+        public override string ToString()
+        {
+            return $"[사원: {Empno}]";
+        }
+    }
+    class EmpTest
+    {
+        static void Main(string[] args)
+        {
+            Emp e = new Emp();  //e는 객체 참조 변수
+            e.Empno = 999;//set
+            Console.WriteLine(e);
+            Console.WriteLine(e.Empno);//get
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
