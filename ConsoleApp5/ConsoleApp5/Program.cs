7258d33c23815d8b0250d6eeb57829227ee2d921
Sunghyun Park <cmos00@nate.com>
2019-02-10 14:18:35 +0900

    20190210 다형성

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public virtual void jitda()
+        {
+            Console.WriteLine(Name + "가 짖다.");
+        }
+    }
+
+    public class Pudle : Dog
+    {
+        public override void jitda()
+        {
+            Console.WriteLine(Name + "푸들푸들~");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine(Name + "가 일한다.");
+        }
+    }
+
+    public class jindo : Dog
+    {
+        public override void jitda()
+        {
+            Console.WriteLine(Name + "진도진도~");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class DogManager
+    {
+        static void Main()
+        {
+            Dog p = new Pudle();
+            p.Name = "푸들이";
+            p.jitda();
+            ((Pudle)p).Work();
+
+
+            Dog j = new jindo();
+            j.Name = "진도이";
+            j.jitda();
+            ((jindo)j).Run();
+        }
+
+    }
+
+}
