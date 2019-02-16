f55ad2b6c770f8f2a9e012dc18f34ffc6400302c
Sunghyun Park <cmos00@nate.com>
2019-02-16 10:06:07 +0900

    20190216 일반화클래스

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,51 @@
+﻿using GenericArray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArray
+{
+    class MyArray<T>
+    {
+        private T[] onj;
+        public MyArray(int size)
+        {
+            onj = new T[size];
+        }
+
+        public void SetElement(int index, T value)
+        {
+            onj[index] = value;
+        }
+
+        public T GetElement(int index, T value)
+        {
+            return onj[index];
+        }
+
+        public void PrintElements()
+        {
+            foreach (T o in onj)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+
+    class MainApp
+    {
+        public static void Main(string[] args)
+        {
+            MyArray<string> array = new MyArray<string>(4);
+
+            array.SetElement(0, "OnjOracleJava");
+            array.SetElement(1, "OracleJavaCommunity");
+            array.SetElement(2, "onjprogramming.co.kr");
+            array.SetElement(3, "oraclejavanew.kr");
+            array.PrintElements();
+        }
+    }
+
+}
--- DeepCopy/DeepCopy/Program.cs
+++ DeepCopy/DeepCopy/Program.cs
@@ -5,13 +5,17 @@ class CreaditCard
     public string name;
 }
 
-class Customer
+class Customer : ICloneable
 {
     public int age;
     public CreaditCard card;
-    public object ShallowCopy()
+    public object Clone()
     {
-        return this.MemberwiseClone();
+        Customer c = new Customer();
+        c.age = this.age;
+        c.card = new CreaditCard();
+        c.card.name = this.card.name;
+        return c;
     }
 }
 
@@ -23,7 +27,7 @@ class ArrayTest
         c1.age = 20;
         c1.card = new CreaditCard();
         c1.card.name = "비씨카드";
-        Customer c2 = (Customer)c1.ShallowCopy();
+        Customer c2 = (Customer)c1.Clone();
         c2.card.name = "BC카드";
 
         Console.WriteLine("c1.card.name = " + c1.card.name);
