a9f3cdea25dfbfbb23f65ae7d6ecab84cb75cbe3
Sunghyun Park <cmos00@nate.com>
2019-02-16 14:23:51 +0900

    20190216 델리게이트 예제

--- ConsoleApp24/ConsoleApp24/Program.cs
+++ ConsoleApp24/ConsoleApp24/Program.cs
@@ -1,16 +1,47 @@
-﻿using System;
-public class Delegate1
+﻿//using System;
+//public class Delegate1
+//{
+//   // private delegate int OnjSum(int i, int j); //1. 선언
+//    public static void Main(string[] args)
+//    {
+//        //Func<int, int, int> myMethod = new Func<int, int, int>(Sum); //2. 생성, 메소드이름을 인자로
+//        // Action<int, int> myMethod = new Action<int, int>(Sum); //2. 생성, 메소드이름을 인자로
+//        Action<int, int> myMethod = Sum;
+//        myMethod(10, 30);
+//    }
+//    static void Sum(int i, int j)
+//    {
+//        System.Console.WriteLine(i + j);
+//    }
+//}
+
+
+using System;
+delegate int SumDeli(int i, int j);
+class DeliTest
 {
-   // private delegate int OnjSum(int i, int j); //1. 선언
-    public static void Main(string[] args)
+    static void Main()
     {
-        Func<int, int, int> myMethod = new Func<int, int, int> (Delegate1.Sum); //2. 생성, 메소드이름을 인자로
-                                                     //OnjSum myMethod = new OnjSum(Sum); //2. 생성
-                                                     //OnjSum myMethod = Sum; //2. 생성
-        Console.WriteLine("두수 합 : {0}", myMethod(10, 30));
+        SumDeli s1 = DeliTest.Sum1;
+        SumDeli s2 = new SumDeli(DeliTest.Sum2);
+        S(s1);
+        S(s2);
     }
-    static int Sum(int i, int j)
+
+    private static void S(SumDeli s)
+    {
+        Console.WriteLine(s(1,2));
+    }
+
+    private static int Sum1(int i, int j)
     {
         return i + j;
     }
-}
+
+    private static int Sum2(int i, int j)
+    {
+        return i + j + 100;
+    }
+
+   
+}
