345446527cc034d85788dbad71923f3ab5a73963
Sunghyun Park <cmos00@nate.com>
2019-02-16 13:44:19 +0900

    20190216 어트리뷰트

---
+++ ConsoleApp22/ConsoleApp22/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeTest
+{
+    [AdditionalInfoAttribute("홍길동", "2018/10/1", Downdload ="http://ojc.asia")]
+    class Test
+    {
+        static void Main()
+        {
+            Type type = typeof(Test);
+            foreach(Attribute attr in type.GetCustomAttributes(true))
+            {
+                AdditionalInfoAttribute info = attr as AdditionalInfoAttribute;
+                if (info != null)
+                    Console.WriteLine("Name={0}, Update={1}," + "DownLoad={2}", info.Name, info.Update, info.Downdload);
+            }
+        }
+    }
+}
