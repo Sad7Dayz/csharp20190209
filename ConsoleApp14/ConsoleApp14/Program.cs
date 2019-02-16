f1af3dd245e704aea9fd10f0ffbae1a3fce8faf7
Sunghyun Park <cmos00@nate.com>
2019-02-16 10:39:05 +0900

    20160216 과제

--- ConsoleApp14/ConsoleApp14/Program.cs
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -1,32 +1,145 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GenericMethod
+using System.Collections;
+
+using System.Collections.Generic;
+
+namespace ConsoleApplication6
+
 {
-    class MainApp
+
+    class Stack1
+
     {
-        static T[] GetArray<T>(int size, T val)
+
+        int top = 0;
+
+        int[] ar = new int[10];
+
+        public void Push(int obj)
         {
-            T[] array = new T[size];
-            for(int i = 0; i < size; i++)
-            {
-                array[i] = val;
-            }
-            return array;
+
+            ar[top] = obj;
+
+            top++;
+
         }
 
-        static void Main(string[] args)
-        {
-            string[] str_array = GetArray<string>(3, "오라클자바커뮤니티, 오라클자바커뮤니티");
-            foreach (string s in str_array)
-                Console.WriteLine(s);
+        public int Pop()
 
-            int[] int_array = GetArray<int>(3, 999);
-            foreach (int i in int_array)
-                Console.WriteLine(i);
+        {
+
+            top--;
+
+            return ar[top];
+
+        }
+
+    }
+
+
+
+    class Stack2
+
+    {
+
+        int top = 0;
+
+        string[] ar = new string[10];
+
+        public void Push(string obj)
+
+        {
+
+            ar[top] = obj;
+
+            top++;
+
+        }
+
+        public string Pop()
+
+        {
+
+            top--;
+
+            return ar[top];
+
+        }
+
+    }
+
+    class Stack3<T>
+    {
+        int top = 0;
+
+        T[] ar = new T[10];
+        public void Push(T obj)
+        {
+            ar[top] = obj;
+            top++;
+        }
+        public T Pop()
+        {
+            top--;
+            return ar[top];
         }
     }
+
+    class StackTest
+
+    {
+        static void Main()
+
+        {
+
+            Stack1 s1 = new Stack1();
+
+            s1.Push(1);
+
+            s1.Push(2);
+
+            s1.Push(3);
+
+            Console.WriteLine(s1.Pop());
+
+            Console.WriteLine(s1.Pop());
+
+            Console.WriteLine(s1.Pop());
+
+
+
+            Stack2 s2 = new Stack2();
+
+            s2.Push("KOREA");
+
+            s2.Push("대한민국");
+
+            s2.Push("서울");
+
+            Console.WriteLine(s2.Pop());
+
+            Console.WriteLine(s2.Pop());
+
+            Console.WriteLine(s2.Pop());
+
+
+            Stack3<string> s3 = new Stack3<string>();
+
+            s3.Push("KOREA");
+
+            s3.Push("대한민국");
+
+            s3.Push("서울");
+
+            Console.WriteLine(s3.Pop());
+
+            Console.WriteLine(s3.Pop());
+
+            Console.WriteLine(s3.Pop());
+
+        }
+
+    }
+
 }
