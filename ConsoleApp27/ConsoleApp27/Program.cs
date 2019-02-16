13f949d8902f01229c5613cbc63a976c5d754f58
Sunghyun Park <cmos00@nate.com>
2019-02-16 16:14:06 +0900

    20190216 이벤트 예제

---
+++ ConsoleApp27/ConsoleApp27/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class EventPublisher
+    {
+        public delegate void MyEventHandler();
+        public event MyEventHandler MyEvent;
+        public void Do()
+        {
+            MyEvent?.Invoke();
+        }
+    }
+
+    class Subscriber
+    {
+        static void Main(string[] args)
+        {
+            EventPublisher p = new EventPublisher();
+
+            p.MyEvent += new EventPublisher.MyEventHandler(doAction);
+
+            p.Do();
+        }
+
+        private static void doAction()
+        {
+            Console.WriteLine("MyEvent라는 이벤트 발생...");
+        }
+    }
+}
