ca075cdc5b02053d063bb30259ffe47748817d4d
Sunghyun Park <cmos00@nate.com>
2019-02-17 15:09:57 +0900

    20190217 멀티쓰레드 에코서버

---
+++ ConsoleApp43/ConsoleApp43/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+class ClientHadler
+{
+    Socket socket = null;
+    NetworkStream stream = null;
+    StreamReader reader = null;
+    StreamWriter writer = null;
+
+    public ClientHadler(Socket socket)
+    {
+        this.socket = socket;
+    }
+
+    public void chat()
+    {
+        stream = new NetworkStream(socket);
+        Encoding encode = Encoding.GetEncoding("utf-8");
+
+        reader = new StreamReader(stream, encode);
+        writer = new StreamWriter(stream, encode)
+        { AutoFlush = true };
+
+        while(true)
+        {
+            string str = reader.ReadLine();
+            Console.WriteLine(str);
+
+            writer.WriteLine(str);
+        }
+    }
+}
+
+class Server
+{
+    public static void Main()
+    {
+        TcpListener tcpListener = null;
+        Socket clientsocket = null;
+
+        try
+        {
+            IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+
+            tcpListener = new TcpListener(ipAd, 5001);
+            tcpListener.Start();
+
+            while(true)
+            {
+                clientsocket = tcpListener.AcceptSocket();
+
+                ClientHadler cHandler = new ClientHadler(clientsocket);
+                Thread t = new Thread (new ThreadStart(cHandler.chat));
+                t.Start();
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        finally
+        {
+            clientsocket.Close();
+        }
+    }
+}
