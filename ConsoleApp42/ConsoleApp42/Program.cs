5ab782d1023ed562524e30f29fb37f58368152b4
Sunghyun Park <cmos00@nate.com>
2019-02-17 14:36:33 +0900

    20190217 에코 클라이언트

---
+++ ConsoleApp42/ConsoleApp42/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpClientTest
+{
+    static void Main(string[] args)
+    {
+        TcpClient client = null;
+
+        try
+        {
+            client = new TcpClient();
+            client.Connect("127.0.0.1", 5001);
+            NetworkStream stream = client.GetStream();
+
+            Encoding encode = Encoding.GetEncoding("utf-8");
+            StreamReader reader = new StreamReader(stream, encode);
+            StreamWriter writer = new StreamWriter(stream, encode)
+            {
+                AutoFlush = true
+            };
+
+            string dataToSend = Console.ReadLine();
+
+            while (true)
+            {
+                writer.WriteLine(dataToSend);
+                String str = reader.ReadLine();
+                Console.WriteLine(str);
+
+                if (dataToSend.IndexOf("<EOF>") > -1)
+                    break;
+                dataToSend = Console.ReadLine();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
